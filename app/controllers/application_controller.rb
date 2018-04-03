287c503aac699a56061cf7769a03e0510523d812
EC2 Default User <ec2-user@ip-172-31-11-233.us-east-2.compute.internal>
2018-04-03 06:14:35 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,15 @@ class ApplicationController < ActionController::Base
   
   include SessionsHelper
 
-  
-  privete
-  
+  private
+
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
+  end
+
+  def counts(user)
+    @count_microposts = user.microposts.count
   end
 end
